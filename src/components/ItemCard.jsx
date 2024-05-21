c13871e859b20ca964e854f0fe32c03f74b281eb
MilosGlamocak <milosglamocak@gmail.com>
2024-05-21 13:22:01 +0200

    Styling updates for admin input fields in shop

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react'
 import '../styles/ItemCard.css'
 import { CircularProgress, Container } from '@mui/material'
 import CustomButton from './CustomButton'
+import CustomInput from './CustomInput'
 import { useAuth, useItems } from '../store'
 import { deleteItem, getAllItems, getAllCarItems, deleteCarItem } from '../../lib/appwrite'
 import LoadingAnimation from './LoadingAnimation'
@@ -24,6 +25,17 @@ function ItemCard({name, price, quantity, imageUrl, itemId, publisher, productCo
     addCartItem(newItem);
 };
 
+  const handleUpdateItem = (e) => {
+    const {value, id} = e.target;
+    console.log(value, id)
+  }
+
+  const itemInfo = [
+    {title: 'Product Code: ', id: 'productCode', value: productCode, type: 'text', className: 'productCode'},
+    {title: 'Price per Unit ($): ', id: 'price', value: price, type: 'number'},
+    {title: 'In Stock: ', id: 'quantity', value: quantity, type: 'number'},
+  ]
+
 const {label} = useAuth((state) => state)
 
   return (
@@ -32,9 +44,13 @@ const {label} = useAuth((state) => state)
             <img src={imageUrl} className='itemImg'/>
             <Container className='cardTextLeft'>
                 <h2 className='cardName'>{name}</h2>
-                <Container className='cardInfoCont'><p className='cardInfo'>Product Code:</p><p className='cardInfoBold'>{productCode}</p></Container>
-                <Container className='cardInfoCont'><p className='cardInfo'>Price per Unit:</p><p className='cardInfoBold'>${price}</p></Container>
-                <Container className='cardInfoCont'><p className='cardInfo'>In Stock: </p><p className='cardInfoBold'>{quantity}</p></Container>
+                {itemInfo.map((item) => {
+                  return (
+                    <Container className='cardInfoCont' key={item.id}><p className='cardInfo'>{item.title} </p>{label === 'admin' ? (
+                      <CustomInput id={item.id} value={item.value} className={`adminItemInput ${item.className}`} onChange={handleUpdateItem} type={item.type}/>
+                    ): (<p className='cardInfoBold'>{item.value}</p>)}</Container>
+                  )
+                })}
                 <Container className='cardInfoCont'><p className='cardInfo'>Added by: </p><p className='cardInfoBold'>{publisher}</p></Container>  
             </Container>
         </Container> 
