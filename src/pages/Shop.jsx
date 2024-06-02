404a5f6c38766d6484b1003c748321891f312334
MilosGlamocak <milosglamocak@gmail.com>
2024-06-02 14:18:18 +0200

    Added logic / now need to improve UX in shop with loading animation

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react'
 import '../styles/Shop.css'
 import {Container} from '@mui/material'
 import {useAuth, useItems} from '../store'
-import { getAllItems, createNewItem, getAllCarItems } from '../../lib/appwrite'
+import { getAllItems, createNewItem, getAllCarItems, getCarItemsCategory } from '../../lib/appwrite'
 import ItemCard from '../components/ItemCard'
 import LoadingAnimation from '../components/LoadingAnimation'
 
@@ -11,20 +11,34 @@ function Shop() {
   /*let [items, setItems] = useState([])*/
 
   let items = useItems((state) => state.items)
+  let [loading, setLoading] = useState(true)
 
-  useEffect(() => {
+  useEffect(() => { 
     //getAllItems()
-    getAllCarItems()
-  }, [])
+    getAllCarItems().finally(() => setLoading(false))
+  }, []) 
+
+  const handleGetItemsCategory = (cat) => {
+    setLoading(true)
+    getCarItemsCategory(cat).finally((setLoading(false)))
+  }
 
   const {label} = useAuth((state) => state)
+
+  const categories = ['Filters', 'Belts', 'Oil', 'Brakes', 'Livery', 'Electronics', 'Wheels', 'Suspension', 'Miscellaneous'];
   
   return (
     <Container className='armoryCont'>
+      <Container className='categoriesCont'>
+        <h3 className='categoryBtn' onClick={() => getAllCarItems()}>All</h3>
+        {categories.map((cat) => {
+          return (<h3 className='categoryBtn' key={cat} onClick={() => handleGetItemsCategory(cat)}>{cat}</h3>)
+        })}
+      </Container>
       <Container className='itemsCont'>
         { items.length > 0 ? items.map((item) => {
           return <ItemCard name={item.name} chamber={item.chamber} imageUrl={item.imageUrl} price={(item.price).toFixed(2)} quantity={item.quantity} key={item.name} itemId={item.$id} publisher={item.users.username} productCode={item.productCode}/>
-        })  : (<LoadingAnimation />)}
+        })  : (loading ? <LoadingAnimation /> : <h2>No items match your search</h2>)}
         
       </Container>
       
