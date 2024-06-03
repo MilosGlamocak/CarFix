d1c866c51cda90944aa1cc3e389cfe2cc46b8390
MilosGlamocak <milosglamocak@gmail.com>
2024-06-03 10:26:54 +0200

    Imported modal from mui

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from '@mui/material'
 import '../styles/Cart.css'
 import { useItems } from '../store'
 import CartItemCard from '../components/CartItemCard'
 import CustomButton from '../components/CustomButton'
+import CheckoutModal from '../components/CheckoutModal'
 
 function Cart() {
+
+  let [open, setOpen] = useState(false)
 
     const cartItems = useItems((state) => state.cartItems)
     let totalCost = 0;
 
-    const handleAddItem = () => {
-      
+    const handleOpenClose = () => {
+      setOpen(!open)
     }
 
   return (
@@ -22,7 +25,8 @@ function Cart() {
             return <CartItemCard imageUrl={item.imageUrl} name={item.name} price={item.price} key={item.itemId} itemId={item.itemId} quantity={item.units} productCode={item.productCode}/>
         }) : (<h3>No items in cart</h3>)}
         <p>Total cost: ${(Math.round(totalCost * 100)/100).toFixed(2)}</p>
-        <CustomButton text='Proceed to Checkout' border='1px solid #365F22' backgroundColor='#365F22' height='3rem' onClick={handleAddItem}/>
+        <CustomButton text='Proceed to Checkout' border='1px solid #365F22' backgroundColor='#365F22' height='3rem' onClick={handleOpenClose}/>
+        <CheckoutModal open={open} handleClose={handleOpenClose}/>
         </Container>
         
     </Container>
